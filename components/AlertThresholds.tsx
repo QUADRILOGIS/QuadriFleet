f4a16fd4ba700a65484ea4b81e9ae9a1df023162
Eno cki <enocki.9@outlook.fr>
2026-02-04 19:09:06 +0100

    [Fix] desktop grid pour la page param

--- components/AlertThresholds.tsx
+++ components/AlertThresholds.tsx
@@ -96,7 +96,7 @@ export default function AlertThresholds({
 
   return (
     <section className="space-y-6">
-      <div className="grid gap-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5">
         {pieces.map((piece) => (
           <div
             key={piece.id}
